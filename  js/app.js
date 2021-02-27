0ba3083835fbf7c2d55923a6793a97f72c474643
ahmad-ahmad <aa0796780751@gmail.com>
2021-02-27 16:53:04 +0200

    add event

---  js/app.js
+++  js/app.js
@@ -7,12 +7,7 @@ function getRandomArbitrary(min, max) {
 const shopSalmon = document.getElementById('shopSalmon');
 let countries = [];
 
-console.log(countries);
-/*0: Salmon {name: "Seattle", min: 23, max: 65, avg: 6.3,.cookiesSales: Array(14), …}
-1: Salmon {name: "Tokoy", min: 3, max: 24, avg: 1.2, cookiesSales: Array(14), …}
-2: Salmon {name: "Dubai", min: 11, max: 38, avg: 3.7, cookiesSales: Array(14), …}
-3: Salmon {name: "Paris", min: 20, max: 38, avg: 3.7, cookiesSales: Array(14), …}
-4: Salmon {name: "Lima", min: 2, max: 16, avg: 4.6, cookiesSales: Array(14), …}*/
+
 
 function Salmon(name, min, max, avg) {
   this.name = name;
@@ -46,48 +41,19 @@ Salmon.prototype.getsales = function () {
   }
 };
 
-// Salmon.prototype.render = function () {
 
-
-
-
-//   // const articalElement = document.createElement('article');
-//   // parentElement.appendChild(articalElement);
-
-//   // const h2Element = document.createElement('h2');
-//   // articalElement.appendChild(h2Element);
-//   // h2Element.textContent = this.name;
-
-//   // const ulElement = document.createElement('ul');
-//   // articalElement.appendChild(ulElement);
-
-//   // console.log('test');
-
-//   // for (let i = 0; i < hour.length; i++) {
-//   //   const liElement = document.createElement('li');
-//   //   ulElement.appendChild(liElement);
-
-//   //   liElement.textContent = `${hour[i]}:${this.cookiesSales[i]} cookies .`;
-
-
-//   // }
-//   // const liElement = document.createElement('li');
-//   // ulElement.appendChild(liElement);
-//   // liElement.textContent = `Total: ${this.total} cookies`;
-
-// };
 
 
 const seattle = new Salmon('Seattle', 23, 65, 6.3);
-//seattle.render();
+
 const tokoy = new Salmon('Tokoy', 3, 24, 1.2);
-//tokoy.render();
+
 const dubai = new Salmon('Dubai', 11, 38, 3.7);
-//dubai.render();
+
 const Paris = new Salmon('Paris', 20, 38, 3.7);
-//Paris.render();
+
 const lima = new Salmon('Lima', 2, 16, 4.6);
-//lima.render();
+
 
 
 
@@ -99,12 +65,6 @@ articalElement.appendChild(tableElement);
 
 
 const headtable = function () {
-
-
-  //creat tr and append it to the table
-  // creat th (name of countries) for the header and append it to the tr
-  // creat for loop and inside it th for the hours
-  // creat th (daily of ...) for the header and append it to the tr
 
   const trElement = document.createElement('tr');
   tableElement.appendChild(trElement);
@@ -123,15 +83,11 @@ const headtable = function () {
 
 };
 
-// call it in the render method
+
 
 
 const bodyTable = function () {
-  //for[i] to loop throu countries
-  // tr
-  // td = countries[i].name
-  // for[j] to loop throu getsales
-  // td = countries[i].getsales[j]
+
 
   for (let i = 0; i < countries.length; i++) {
 
@@ -176,16 +132,13 @@ const footerTable = function () {
 
     for (let j = 0; j < countries.length; j++) {
       console.log(countries[j].cookiesSales[i]);
-      // totalPerHour = totalPerHour + (countries[j].cookiesSales[i]);
+
       totalPerHour += countries[j].cookiesSales[i];
-      // console.log("totalPerHour", totalPerHour);
+
 
     }
     th2Element.textContent = totalPerHour;
-    // th2Element.textContent =countries[i].totalPerHour;
-    //  th2Element.textContent =countries[i].cookiesSales;
-    // countries[i].totalperhourallcontury += countries[i].cookiesSales;
-    // th2Element.textContent = countries[i].cookiesSales;
+
 
   }
   const th3Element = document.createElement('th');
@@ -199,8 +152,6 @@ const footerTable = function () {
 };
 
 
-
-//console.log(countries);
 
 
 const formElement = document.getElementById('addNewCountryForm');
@@ -219,7 +170,7 @@ formElement.addEventListener('submit', function (event) {
 
 
   const country = new Salmon(countryName, minCustomer, maxCustomer, avgCookies);
-  //document.getElementById('shopSalmon').removeChild(document.getElementById('shopSalmon').lastChild);
+
 
   const trNewElement = document.createElement('tr');
   tableElement.appendChild(trNewElement);
